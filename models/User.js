60ecd6729b72eb1a0b842bed5e7b4badcf010ee5
Kaylan Prophet <kaylan.prophet@gmail.com>
2022-07-16 12:33:24 -0400

    created user model

---
+++ models/User.js
@@ -0,0 +1,23 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: 'Username is required',
+        trim: true
+    },
+
+    email: {
+type: String,
+required: 'Password is required',
+Unique: true,
+match: [/.+@.+\..+/]
+},
+thoughts: [{type: Schema.Types.ObjectId, ref: 'Thoughts'}],
+friends: [{type: Schema.Types.ObjectId, ref: 'User'}]
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
